092c05094fa4e724f8bce8a23ab246d6a58d4d4c
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-31 12:31:49 +0700

    fix api

--- config/services.php
+++ config/services.php
@@ -2,66 +2,66 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Third Party Services
+  |--------------------------------------------------------------------------
+  |
+  | This file is for storing the credentials for third party services such
+  | as Mailgun, Postmark, AWS and more. This file provides the de facto
+  | location for this type of information, allowing packages to have
+  | a conventional file to locate the various service credentials.
+  |
+  */
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+  'postmark' => [
+    'token' => env('POSTMARK_TOKEN'),
+  ],
 
-    'resend' => [
-        'key' => env('RESEND_KEY'),
-    ],
+  'resend' => [
+    'key' => env('RESEND_KEY'),
+  ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+  'ses' => [
+    'key' => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+  ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
+  'slack' => [
+    'notifications' => [
+      'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+      'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
     ],
+  ],
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URL'),
-    ],
+  'google' => [
+    'client_id' => env('GOOGLE_CLIENT_ID'),
+    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    'redirect' => env('GOOGLE_REDIRECT_URL'),
+  ],
 
-    'facebook' => [
-        'client_id'     => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_REDIRECT_URI'),
-    ],
-    
-    'gemini' => [
-        'api_keys' => env('GEMINI_API_KEYS'),
-    ],
+  'facebook' => [
+    'client_id' => env('FACEBOOK_CLIENT_ID'),
+    'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+    'redirect' => env('FACEBOOK_REDIRECT_URI'),
+  ],
 
-    'removebg' => [
-        'api_key' => env('REMOVEBG_API_KEY'),
-        'api_url' => 'https://api.remove.bg/v1.0/removebg',
-    ],
+  'gemini' => [
+    'api_key' => env('GEMINI_API_KEY'),
+  ],
 
-    'ml_microservice' => [
-        'url' => env('ML_MICROSERVICE_URL', 'http://localhost:8001'),
-    ],
+  'removebg' => [
+    'api_key' => env('REMOVEBG_API_KEY'),
+    'api_url' => 'https://api.remove.bg/v1.0/removebg',
+  ],
 
-    'serpapi' => [
-        'api_key' => env('SERPAPI_API_KEY'),
-        'base_url' => 'https://serpapi.com/search',
-    ],
+  'ml_microservice' => [
+    'url' => env('ML_MICROSERVICE_URL', 'http://localhost:8001'),
+  ],
+
+  'serpapi' => [
+    'api_key' => env('SERPAPI_API_KEY'),
+    'base_url' => 'https://serpapi.com/search',
+  ],
 ];
